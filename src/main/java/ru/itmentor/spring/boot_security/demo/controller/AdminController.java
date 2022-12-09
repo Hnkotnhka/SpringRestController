d60390f788d5104d40b85352be1710639b22acf2
onadych@usetech.ru <onadych@usetech.ru>
2022-12-09 10:58:26 +0500

    First

---
+++ src/main/java/ru/itmentor/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,49 @@
+package ru.itmentor.spring.boot_security.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.itmentor.spring.boot_security.demo.model.User;
+import ru.itmentor.spring.boot_security.demo.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserService service;
+
+    public AdminController (UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<User> showAllUsers() {
+        List <User> allUsers = service.getList();
+        return allUsers;
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById (@PathVariable Long id){
+        User users = service.read(id);
+        return users;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        service.create(user, user.getRoles().toArray(new String[0]));
+        return ResponseEntity.ok(user);
+    }
+    @PutMapping
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        service.update(user, user.getRoles().toArray(new String[0]));
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping ("/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        service.delete(getUserById(id));
+        return "User deleted";
+    }
+
+}
---
+++ src/main/java/ru/itmentor/spring/boot_security/demo/dao/UserDaoImp.java
@@ -0,0 +1,69 @@
+package ru.itmentor.spring.boot_security.demo.dao;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.itmentor.spring.boot_security.demo.model.Role;
+import ru.itmentor.spring.boot_security.demo.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+@Transactional
+public class UserDaoImp implements UserDao {
+
+   @PersistenceContext
+   private EntityManager manager;
+
+   @Override
+   public void create(User user, String[] selectedRoles) {
+      manager.persist(createUser(user, selectedRoles));
+   }
+
+   @Override
+   public User read(Long id) {
+      return manager.find(User.class, id);
+   }
+
+   @Override
+   public User getByName(String name) {
+      return manager.createQuery("select u from User u where u.username = :name", User.class)
+              .setParameter("name", name)
+              .getSingleResult();
+   }
+
+   @Override
+   public void update(User user, String[] selectedRoles) {
+      manager.merge(createUser(user, selectedRoles));
+   }
+
+   @Override
+   public void delete(User user) {
+      manager.remove(manager.find(User.class, user.getId()));
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> getList() {
+      return manager.createQuery("from User").getResultList();
+   }
+
+   @Override
+   public List<Role> getRoles() {
+      return manager.createQuery("from Role", Role.class).getResultList();
+   }
+
+   private User createUser(User user, String[] selectedRoles) {
+      List<String> roleNames = Arrays.asList(selectedRoles);
+      List<Role> allRoles = getRoles();
+
+      for (Role role : allRoles) {
+         if (roleNames.contains(role.getName())) {
+            user.addRole(role);
+         }
+      }
+      return user;
+   }
+}
